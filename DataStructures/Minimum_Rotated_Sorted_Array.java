c335e3673315878e7c453a9c46b4087054196307
Soumojit Ghosh <ghoshsoumojit53@gmail.com>
2023-10-07 11:26:37 +0530

    Day 6

---
+++ DataStructures/Minimum_Rotated_Sorted_Array.java
@@ -0,0 +1,26 @@
+package DataStructures;
+
+public class Minimum_Rotated_Sorted_Array {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            if (nums[low] < nums[high])
+                return nums[low];
+            int mid = low + (high - low) / 2;
+            if (nums[low] <= nums[mid])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return nums[low];
+    }
+}
+
+
+// Note
+// 1. Make a binary search with the following algorithm
+// 	a. If nums[low] < nums[high] , we hacve the minimum at nums[low]
+// 	b. if nums[low] <= nums[mid] the left array is sorted and we need to move towards right else we move left
