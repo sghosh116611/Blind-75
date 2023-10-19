f08fa98b1ecd1bc1a7a4faf1a897ddc45209fd33
Soumojit Ghosh <ghoshsoumojit53@gmail.com>
2023-10-19 13:22:38 +0530

    day 17

---
+++ NonLinearDataStructure/Max_Path_Sum.java
@@ -0,0 +1,14 @@
+public class Max_Path_Sum {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxPathDown(root);
+        return max;
+    }
+    public int maxPathDown(TreeNode node){
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        max = Math.max(max, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}
